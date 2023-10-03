6edb4b4a3cddb08c29a969d80a96c7b69e9620cc
Sohail <20193061.itsohail@gmail.com>
2023-10-03 18:43:58 +0530

    Added card details page with validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+
+      <div className='container'>
+        <div className='leftside'></div> 
+        <div className='rightside'></div>
+      </div>
   );
 }
 
